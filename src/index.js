fae02e81825f11f5dcca77cd2266ed3fb44a4e98
SamFaMH <121002154+SamFaMH@users.noreply.github.com>
2024-12-28 13:45:43 +0530

    Finished

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from './App';
+
+// Get the root element
+const container = document.getElementById('root');
+
+// Create a root and render the App component
+const root = createRoot(container);
+root.render(<App />);
